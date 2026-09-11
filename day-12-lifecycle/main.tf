967459d53f89236ae9592da1b2cccf000c35e38b
anjalikumbhar372 <kumbharanjali070@gmail.com>
2026-09-11 22:56:30 +0530

    lifecycle

--- day-12-lifecycle/main.tf
+++ day-12-lifecycle/main.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "s3" {
-    bucket = "anjali_bucket_demo_2529"
+    bucket = "anjali-bucket-demo-2529"
 
     lifecycle {
         prevent_destroy = true
     }
 
     tags = {
-        Name = "anjali_bucket_demo_2529"
+        Name = "anjali-bucket-demo-2529"
     }
 }
